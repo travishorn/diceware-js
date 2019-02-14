6b2acf3694b1a656821d891ba49f7e5918ca59b4
Travis Horn <travis@travishorn.com>
2019-02-13 19:22:19 -0600

    Process options in separate function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const randomNumber = require('random-number-csprng');
 const lists = require('./lists');
 
+const processOptions = (...rest) => {
+  const options = Object.assign({}, ...rest);
+
+  options.words = parseInt(options.words, 10);
+  options.list = lists.find(l => l.name === options.list);
+
+  if (options.words <= 0) throw new Error(`Unsupported number of words: ${options.words}. Please choose an integer greater than 0.`);
+
+  if (typeof options.list === 'undefined') {
+    const listNames = lists.map(l => l.name).join(', ');
+    throw new Error(`No list exists with name: ${options.list}. Please choose from: ${listNames}`);
+  }
+
+  return options;
+};
+
 const rollDice = (num) => {
   const rolls = [];
 
@@ -17,24 +33,17 @@ const generate = async (passedOptions) => {
     list: 'Arnold G. Reinhold',
   };
 
-  const options = Object.assign(defaults, passedOptions);
-
-  const list = lists.find(l => l.name === options.list);
-
-  if (typeof list === 'undefined') {
-    const listNames = lists.map(l => l.name).join(', ');
-    throw new Error(`No list exists with name: ${options.list}. Please choose from: ${listNames}`);
-  }
+  const options = processOptions(defaults, passedOptions);
 
   const prmRolls = [];
 
   while (prmRolls.length < options.words) {
-    prmRolls.push((rollDice(list.diceLength)));
+    prmRolls.push((rollDice(options.list.diceLength)));
   }
 
   const rolls = await Promise.all(prmRolls);
 
-  const passphrase = rolls.map(roll => list.list[roll.join('')]).join(' ');
+  const passphrase = rolls.map(roll => options.list.list[roll.join('')]).join(' ');
 
   return passphrase;
 };
