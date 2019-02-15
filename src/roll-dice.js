d7c21574cb18513e236902ba39647c7229c91a18
Travis Horn <travis@travishorn.com>
2019-02-14 19:59:16 -0600

    Remove redundant validation

--- src/roll-dice.js
+++ src/roll-dice.js
@@ -1,11 +1,6 @@
-const Joi = require('joi');
 const randomNumber = require('random-number-csprng');
 
 const rolls = (num) => {
-  const schema = Joi.number().min(1).required();
-  const valid = Joi.validate(num, schema);
-  if (valid.error) throw valid.error;
-
   const result = [];
 
   while (result.length < num) {
@@ -16,13 +11,6 @@ const rolls = (num) => {
 };
 
 const sets = async (options) => {
-  const schema = Joi.object().keys({
-    sets: Joi.number().min(1).required(),
-    rollsEach: Joi.number().min(1).required(),
-  });
-  const valid = Joi.validate(options, schema);
-  if (valid.error) throw valid.error;
-
   const prmSets = [];
 
   while (prmSets.length < options.sets) {
