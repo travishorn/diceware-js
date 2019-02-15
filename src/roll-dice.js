49ed8f5e99e62f66298be3b70f84a3206b2dd89a
Travis Horn <travis@travishorn.com>
2019-02-14 19:01:07 -0600

    Fixed bug: Dice rolls not working

--- src/roll-dice.js
+++ src/roll-dice.js
@@ -1,6 +1,11 @@
+const Joi = require('joi');
 const randomNumber = require('random-number-csprng');
 
 const rolls = (num) => {
+  const schema = Joi.number().min(1).required();
+  const valid = Joi.validate(num, schema);
+  if (valid.error) throw valid.error;
+
   const result = [];
 
   while (result.length < num) {
@@ -11,10 +16,17 @@ const rolls = (num) => {
 };
 
 const sets = async (options) => {
+  const schema = Joi.object().keys({
+    sets: Joi.number().min(1).required(),
+    rollsEach: Joi.number().min(1).required(),
+  });
+  const valid = Joi.validate(options, schema);
+  if (valid.error) throw valid.error;
+
   const prmSets = [];
 
   while (prmSets.length < options.sets) {
-    prmSets.push((rolls(options.diceEach)));
+    prmSets.push((rolls(options.rollsEach)));
   }
 
   const result = await Promise.all(prmSets);
